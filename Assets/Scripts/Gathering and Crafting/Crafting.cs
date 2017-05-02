b9ea04600ce32f66e22042aafa613bba7ac8360f
Lars <larsgames11@gmail.com>
2017-05-02 18:00:28 +0200

    new scripts

---
+++ Assets/Scripts/Gathering and Crafting/Crafting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafting : MonoBehaviour {
+
+	public GameObject[] items;
+	public Transform instPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Campfire()
+	{
+		GameObject campfire = Instantiate (items [0], instPosition.position, instPosition.rotation);
+		campfire.transform.SetParent (GameObject.FindWithTag ("Player").transform.FindChild ("MainCamera").transform);
+	}
+}
---
+++ Assets/Scripts/Gathering and Crafting/PlaceItem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceItem : MonoBehaviour {
+
+	public GameObject realItem;
+
+	public Material[] materials;
+
+	public bool canPlace;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.GetComponent<Renderer> ().material == materials[0]) {
+			canPlace = false;
+		} else {
+			canPlace = true;
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Terrain") {
+			gameObject.GetComponent<Renderer> ().material = materials [1];
+			if (Input.GetMouseButtonDown (0) && canPlace) {
+				GameObject item = Instantiate (realItem, transform.position, transform.rotation);
+				Destroy (gameObject);
+			}
+		} else {
+			gameObject.GetComponent<Renderer> ().material = materials [0];
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Terrain") {
+			gameObject.GetComponent<Renderer> ().material = materials [0];
+		}
+	}
+}
